961637591f4c4ea89d4f0c8825c9661523ec5ef2
Matias Aguirre <aguirremati09@gmail.com>
2022-07-19 14:56:37 -0300

    Applied JS to mobile menu

---
+++ main.js
@@ -0,0 +1,28 @@
+// Toggable hamburger menu
+
+const displayedMenu = document.querySelector('.displayedMenu');
+const menuItems = document.querySelectorAll('.menuItem');
+const hamburgerToggle = document.querySelector('.hamburgerToggle');
+const icon = document.querySelector('.icon');
+const closeIcon = document.querySelector('.closeIcon');
+
+function openMenuHam() {
+  if (displayedMenu.classList.contains('openMenu')) {
+    displayedMenu.classList.toggle('openMenu');
+    closeIcon.style.display = 'none';
+    icon.style.display = 'block';
+  } else {
+    displayedMenu.classList.toggle('openMenu');
+    closeIcon.style.display = 'block';
+    icon.style.display = 'none';
+  }
+}
+
+function clickLinks() {
+  displayedMenu.classList.remove('openMenu');
+  closeIcon.style.display = 'none';
+  icon.style.display = 'block';
+}
+
+
+
